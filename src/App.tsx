c43f1d53c79c1d7cef3ecb22ea79abc807110423
[dyad] <dnnabdonbaw@gmail.com>
2025-06-08 21:39:08 +0100

    [dyad] Correction de l'erreur de compilation JSX dans App.tsx en ajoutant l'importation de React. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react'; // Added this line
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
