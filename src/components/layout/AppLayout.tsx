5013a61f197b0d3eb361fc973aaa0c9578627afb
[dyad] <dnnabdonbaw@gmail.com>
2025-06-18 08:56:34 +0100

    [dyad] wrote 1 file(s)

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Sidebar } from './Sidebar';
+import { Header } from './Header';
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -6,8 +8,14 @@ interface AppLayoutProps {
 
 export const AppLayout = ({ children }: AppLayoutProps) => {
   return (
-    <div className="temp-app-layout">
-      {children}
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-6 overflow-y-auto">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
